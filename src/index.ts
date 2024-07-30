03df43da7670aa05be3f99b62a127e67fb767bac
LucasGlave <lucasglave7@gmail.com>
2024-07-30 18:02:42 -0300

    deploy 6

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import routes from "./routes";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import morgan from "morgan";
 
 dotenv.config();
 const app = express();
@@ -13,6 +14,7 @@ const PORT = process.env.PORT;
 const PORT_PROD = process.env.PORT_PROD;
 
 app.use(express.json());
+app.use(morgan("dev"))
 app.use(bodyParser.json());
 app.use(cookieParser());
 
