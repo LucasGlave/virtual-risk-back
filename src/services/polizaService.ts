2d29d6becf326cfd089506ed5e227b2addc03fae
Franco Prandi <francoprandi55@gmail.com>
2024-09-10 11:14:23 -0300

    feat: updates

--- src/services/polizaService.ts
+++ src/services/polizaService.ts
@@ -60,41 +60,44 @@ const deletePolizaByNumber = async (numeroPoliza:string) => {
 const filterPolizas = async (filters: Partial<PolizaProps>, page: number, pageSize: number) => {
   const offset = (page - 1) * pageSize;
   const limit = pageSize;
-    const whereClause: any = {};
-    if (filters.asegurado) {
-      whereClause.asegurado = { [Op.iLike]: `%${filters.asegurado}%` };
-    }
-    if (filters.compañia) {
-      whereClause.compañia = { [Op.iLike]: `%${filters.compañia}%` };
-    }
-    if (filters.detalle) {
-      whereClause.detalle = { [Op.iLike]: `%${filters.detalle}%` };
-    }
-    if (filters.estado) {
-      whereClause.estado = { [Op.iLike]: `%${filters.estado}%` };
-    }
-    if (filters.vigenciaInicio && filters.vigenciaFin) {
-      whereClause.vigenciaInicio = { [Op.gte]: filters.vigenciaInicio };
-      whereClause.vigenciaFin = { [Op.lte]: filters.vigenciaFin };
-    } else if (filters.vigenciaInicio) {
-      whereClause.vigenciaInicio = { [Op.gte]: filters.vigenciaInicio };
-    } else if (filters.vigenciaFin) {
-      whereClause.vigenciaFin = { [Op.lte]: filters.vigenciaFin };
-    }
-    const { count, rows } = await Poliza.findAndCountAll({
-        where: whereClause,
-        order: [['id', 'ASC']],
-        offset: offset,
-        limit: limit
-    });
+  const whereClause: any = {};
 
-    return {
-        totalItems: count,
-        totalPages: Math.ceil(count / pageSize),
-        currentPage: page,
-        pageSize: pageSize,
-        data: rows
-    };
+  if (filters.asegurado) {
+    whereClause.asegurado = { [Op.iLike]: `%${filters.asegurado}%` };
+  }
+  if (filters.compañia) {
+    whereClause.compañia = { [Op.iLike]: `%${filters.compañia}%` };
+  }
+  if (filters.detalle) {
+    whereClause.detalle = { [Op.iLike]: `%${filters.detalle}%` };
+  }
+  if (filters.estado) {
+    whereClause.estado = { [Op.iLike]: `%${filters.estado}%` };
+  }
+  if (filters.vigenciaInicio && filters.vigenciaFin) {
+    whereClause.vigenciaInicio = { [Op.gte]: filters.vigenciaInicio };
+    whereClause.vigenciaFin = { [Op.lte]: filters.vigenciaFin };
+  } else if (filters.vigenciaInicio) {
+    whereClause.vigenciaInicio = { [Op.gte]: filters.vigenciaInicio };
+  } else if (filters.vigenciaFin) {
+    whereClause.vigenciaFin = { [Op.lte]: filters.vigenciaFin };
+  }
+
+  const { count, rows } = await Poliza.findAndCountAll({
+    where: whereClause,
+    order: [['id', 'ASC']],
+    offset,
+    limit,
+  });
+
+  return {
+    totalItems: count,
+    totalPages: Math.ceil(count / pageSize),
+    currentPage: page,
+    pageSize,
+    data: rows,
+  };
 };
+
 
 export default {createPoliza, viewPolizas, viewPolizaByNumber, editPolizaByNumber, deletePolizaByNumber, filterPolizas}
