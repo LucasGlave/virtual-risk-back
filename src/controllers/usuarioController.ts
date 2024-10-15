fbfb11cb9176cafa55790780201aefb4818abfc4
Franco Prandi <francoprandi55@gmail.com>
2024-10-15 10:54:39 -0300

    feat: login update

--- src/controllers/usuarioController.ts
+++ src/controllers/usuarioController.ts
@@ -3,28 +3,40 @@ import userService from "../services/userService";
 
 const loginUser = async (req: Request, res: Response) => {
   const { username, password } = req.body;
+
   if (!username || !password) {
     return res.status(400).send("All fields are required");
   }
+
   try {
     const { payload, token } = await userService.loginUser(username, password);
+
     res.cookie("token", token, {
       sameSite: "none",
       httpOnly: true,
-      secure: true, // en producción cambiar a true
+      secure: true,
       path: "/",
     });
-    res.status(200).send(`User has been logged`);
-  } catch (error) {
-    res.status(500).send(`Error when trying to login user: ${error}`);
+
+    res.status(200).send("User has been logged");
+  } catch (error: any) {
+    if (error.message === "Invalid password") {
+      return res.status(401).send("Invalid password");
+    }
+    if (error.message === "User does not exist") {
+      return res.status(404).send("User does not exist");
+    }
+
+    res.status(500).send(`Error when trying to login user: ${error.message}`);
   }
 };
+
 
 const logoutUser = (req: Request, res: Response) => {
   res.clearCookie("token", {
     sameSite: "none",
     httpOnly: true,
-    secure: true, // en producción cambiar a true
+    secure: true,
     path: "/",
   });
   res.status(200).send("Cookies deleted");
